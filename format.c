bc71c2174922de5dfbefe2865721717b21258d31
Karl Lehenbauer <karl@lehenbauer.com>
2026-05-10 22:29:38 -0500

    Add Whisp tmux protocol version format

--- format.c
+++ format.c
@@ -2725,6 +2725,13 @@ format_cb_version(__unused struct format_tree *ft)
 	return (xstrdup(getversion()));
 }
 
+/* Callback for whisp_tmux_protocol_version. */
+static void *
+format_cb_whisp_tmux_protocol_version(__unused struct format_tree *ft)
+{
+	return (format_printf("%u", WHISP_TMUX_PROTOCOL_VERSION));
+}
+
 /* Callback for sixel_support. */
 static void *
 format_cb_sixel_support(__unused struct format_tree *ft)
@@ -3693,6 +3700,9 @@ static const struct format_table_entry format_table[] = {
 	},
 	{ "version", FORMAT_TABLE_STRING,
 	  format_cb_version
+	},
+	{ "whisp_tmux_protocol_version", FORMAT_TABLE_STRING,
+	  format_cb_whisp_tmux_protocol_version
 	},
 	{ "window_active", FORMAT_TABLE_STRING,
 	  format_cb_window_active
